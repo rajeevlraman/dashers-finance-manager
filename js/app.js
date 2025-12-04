dd2a2b99e800d785ff1bbda5fc1c4e6b35ae612a
Rajeev L Raman <85452477+rajeevlraman@users.noreply.github.com>
2025-12-05 00:57:06 +1100

    revert

--- js/app.js
+++ js/app.js
@@ -11,8 +11,7 @@ import { processRecurringTransactions, processDueBills } from './recurringJob.js
 import { applyLayoutChanges, LayoutModes } from './layoutManager.js';
 //version import 
 import { APP_VERSION } from './version.js';
-
-console.log(`🚀 Dashers Finance Manager v${APP_VERSION}`);
+console.log("🚀 Dashers Finance - Version", APP_VERSION);
 
 
 // Optional check: warn if not HTTPS (affects PWA install prompt)
