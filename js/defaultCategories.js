b5f6bf988879a81a12a5fd7179a020eebd046d99
Rajeev L Raman <85452477+rajeevlraman@users.noreply.github.com>
2025-12-15 11:22:23 +1100

    wool

--- js/defaultCategories.js
+++ js/defaultCategories.js
@@ -72,6 +72,7 @@ export const DEFAULT_CATEGORIES = [
 { id: 'exp_Mal_Vic_Pty', name: 'Meat & Afghani', type: 'expense', icon: '🥩', parentId: 'exp_groceries' },
 
 { id: 'exp_Safeway', name: 'Safeway', type: 'expense', icon: '🧽', parentId: 'exp_groceries' },
+{ id: 'exp_woolworths', name: 'Woolworths', type: 'expense', icon: '🧽', parentId: 'exp_groceries' },
 { id: 'exp_Coles', name: 'Coles', type: 'expense', icon: '🧽', parentId: 'exp_groceries' },
 { id: 'exp_IGA', name: 'IGA', type: 'expense', icon: '🧽', parentId: 'exp_groceries' },
 { id: 'exp_Woolworths', name: 'Woolworths', type: 'expense', icon: '🛒', parentId: 'exp_groceries' },
