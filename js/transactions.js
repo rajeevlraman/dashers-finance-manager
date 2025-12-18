3750b5ec9ace4236e9b7864b32e3285289308a98
Rajeev L Raman <85452477+rajeevlraman@users.noreply.github.com>
2025-12-19 07:26:46 +1100

    add

--- js/transactions.js
+++ js/transactions.js
@@ -56,7 +56,9 @@ export async function initTransactionsUI() {
   mainContent.innerHTML = `
     <div class="page-container">
 
+
       <div class="page-header">
+          <div id="importPanelMount"></div>
         <h2>💸 Transactions</h2>
         <div class="page-actions">
           <button class="btn btn-primary" id="btnAddTx">➕ Add Transaction</button>
